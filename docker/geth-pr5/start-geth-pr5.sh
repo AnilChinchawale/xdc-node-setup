3539aeaceefc4aae9074d38f2b0f2d0cca37d3ad
anilchinchawale <anil24593@gmail.com>
2026-02-25 17:02:12 +0530

    fix: GP5 uses --miner.gasprice and --miner.gaslimit flags
    Old flags --gasprice and --targetgaslimit are not supported in geth 1.17+

--- docker/geth-pr5/start-geth-pr5.sh
+++ docker/geth-pr5/start-geth-pr5.sh
@@ -111,9 +111,9 @@ ARGS="$ARGS --syncmode $SYNC_MODE"
 ARGS="$ARGS --gcmode $GC_MODE"
 ARGS="$ARGS --verbosity $LOG_LEVEL"
 
-# Miner settings (XDC uses different flags than standard geth)
-ARGS="$ARGS --gasprice 1"
-ARGS="$ARGS --targetgaslimit 420000000"
+# Miner settings (GP5 / geth 1.17+ uses --miner.* style flags)
+ARGS="$ARGS --miner.gasprice 1"
+ARGS="$ARGS --miner.gaslimit 420000000"
 
 # Wallet unlock for mining
 if [ -n "$wallet" ] && [ -f "$PWD_FILE" ]; then
